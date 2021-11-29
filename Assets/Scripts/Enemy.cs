8e0e542d7e4c21bb1758d1f7c6fd6f8c38dd789a
Thomas Winkley <thomas.winkley@gmail.com>
2021-11-29 09:13:34 -0700

    static member stuff
    test

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -36,3 +36,4 @@ public class Enemy : MonoBehaviour
 
 
 }
+
